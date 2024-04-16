3df32899af566e80ec9b3fbe6c28dd8cd94bfd9b
HenryGonzalezAlguera <henry_cr999@hotmail.com>
2024-04-16 11:22:08 -0600

    semana 13

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,17 +1,25 @@
 
 package com.tienda.controller;
 
+import com.tienda.service.ItemService;
+import com.tienda.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//Estas son anotaciones arriba de clases y de metodos
 @Controller
 public class IndexController {
     
+    @Autowired
+    ProductoService productoService;
+    @Autowired
+    ItemService itemService;
+    
     @RequestMapping("/")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
+    public String page(Model model) { 
+        var listaProductos = productoService.getProductos(true);
+        model.addAttribute("productos", listaProductos);
         return "index";
     }
     
--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,3 +1,6 @@
+function cargarLista(){    
+    var url = '/refrescarBoton';    
+    $("#resultsBlock").load(url);}
 /* La siguiente función se utiliza para visualizar la imagen seleccionada en la
  * página html donde se desea "cargar" utilizando un llamado "ajax"*/
 function readURL(input) {
